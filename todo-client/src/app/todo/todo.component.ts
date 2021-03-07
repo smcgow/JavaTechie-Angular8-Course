3585b5f8a6baca74d67df266f7325fe417dc9509
smcgow <stepheneoinmcgowan@gmail.com>
2021-03-07 20:06:49 +0000

    Committing first version of the service. Calls online mockaroo api to get a list of todos.

---
+++ todo-client/src/app/todo/todo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {TodoService} from '../todo.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  search = 'hjhjs';
+  amount = 500;
+  todos: any;
+
+  constructor(private service: TodoService) { }
+
+  ngOnInit(): void {
+      this.service.getToDos().subscribe((data) => this.todos = data);
+  }
+
+}
